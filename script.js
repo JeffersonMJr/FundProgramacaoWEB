3125e422949dfa2206073d121489dd3d85e2d75e
JeffersonMJr <jeffersonmjunior.31@gmail.com>
2024-12-08 17:13:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Evento de envio do formulário
+document.getElementById('form-contato').addEventListener('submit', function () {
+
+  // Exibe uma mensagem mostrando que foi enviado
+alert(`Obrigado, Sua mensagem foi enviada com sucesso.`);   
+
+  });
+  
